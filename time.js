0df3fe9959b58caf61eb6f39d27e43abc912cf96
KarenAngelis <karen.denich@gmail.com>
2022-11-07 20:20:53 -0300

    subindo para entregar algo

---
+++ time.js
@@ -0,0 +1,35 @@
+//1 - Pipoca – 10 segundos (padrão);
+//2 - Macarrão – 8 segundos (padrão);
+//3 - Carne – 15 segundos (padrão);
+//4 - Feijão – 12 segundos (padrão);
+//5 - Brigadeiro – 8 segundos (padrão); 
+
+
+// ------- // ------ // ------ // ------ //
+
+//1 - Pipoca – 10 segundos (padrão);
+
+function pipoca (tempo){ //testado ok
+  if (tempo == 10)
+  return "Pronto";
+
+else if (tempo == 10 * 2 ) { //testado ok
+  return "Comida queimou"
+}
+else if (tempo <= 9) { // testado ok
+  return "tempo insuficiente";
+}
+else if (" ") {
+  return "Prato inexistente"; //testado ok
+}
+else if (tempo == 10 * 3 ) { 
+  return "Kabumm"
+}
+else {
+  return "Prato pronto, bom apetite!!!"
+}};
+
+console.log(pipoca(10 * 2));
+
+
+
